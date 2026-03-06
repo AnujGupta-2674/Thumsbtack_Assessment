37846c7befac0c57fd35999fb5af5707bd25be1c
Anuj Bhaskar Gupta <anuj_gupta04@Anujs-MacBook-Air.local>
2026-03-06 16:22:31 +0530

    Added BE url

--- frontend/src/app/redux/actions/api.js
+++ frontend/src/app/redux/actions/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_BASE_URL,
+  baseURL: process.env.NEXT_BASE_URL || "https://thumsbtack-assessment.onrender.com/api/v1",
   withCredentials: true
 });
 
