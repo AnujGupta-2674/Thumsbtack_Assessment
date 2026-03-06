18b78ac35df91473a6f726fdb820164a8d2be85f
Anuj Bhaskar Gupta <anuj_gupta04@Anujs-MacBook-Air.local>
2026-03-06 17:15:20 +0530

    Small Change

--- frontend/src/app/redux/actions/api.js
+++ frontend/src/app/redux/actions/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: process.env.NEXT_BASE_URL || "https://thumsbtack-assessment.onrender.com/api/v1",
-  withCredentials: true
+  // withCredentials: true
 });
 
 export default API;
