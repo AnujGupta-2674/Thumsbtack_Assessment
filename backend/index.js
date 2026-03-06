16be08dc32c633ca8501c2f070d36b16933057aa
Anuj Bhaskar Gupta <anuj_gupta04@Anujs-MacBook-Air.local>
2026-03-06 17:08:40 +0530

    Fixed Cors Issue

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
         origin: '*',
-        credentials: true
+        // credentials: true
     }
 ));
 app.use(cookieParser());
