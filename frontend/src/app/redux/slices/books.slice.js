fc84580b35a4e187ddf1b56e4070d1ca4830ba10
Anuj Bhaskar Gupta <anuj_gupta04@Anujs-MacBook-Air.local>
2026-03-06 16:53:33 +0530

    Real Time Book added functionality

--- frontend/src/app/redux/slices/books.slice.js
+++ frontend/src/app/redux/slices/books.slice.js
@@ -14,23 +14,20 @@ const booksSlice = createSlice({
 
   extraReducers: (builder) => {
     builder
-
       .addCase(fetchBooks.pending, (state) => {
         state.loading = true;
       })
 
       .addCase(fetchBooks.fulfilled, (state, action) => {
         state.loading = false;
+        state.books = action.payload;
+      })
+
+      .addCase(addBook.fulfilled, (state, action) => {
         state.books.unshift(action.payload);
       })
 
-      .addCase(fetchBooks.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      })
-
       .addCase(updateBook.fulfilled, (state, action) => {
-
         const index = state.books.findIndex(
           book => book._id === action.payload._id
         );
@@ -38,11 +35,12 @@ const booksSlice = createSlice({
         if (index !== -1) {
           state.books[index] = action.payload;
         }
-
       })
 
       .addCase(deleteBook.fulfilled, (state, action) => {
-        state.books = state.books.filter(book => book._id !== action.payload);
+        state.books = state.books.filter(
+          book => book._id !== action.payload
+        );
       });
   }
 });
